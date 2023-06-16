64c56a08d4097bfb4a5ed4e729db4217c615e1cf
BassHound <basso.andrea.96@gmail.com>
2023-06-16 13:52:09 +0200

    UI login and bartender updated

--- Server/Code/Routes/tables.routes.js
+++ Server/Code/Routes/tables.routes.js
@@ -25,7 +25,6 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.tablesRouter = void 0;
 const table = __importStar(require("../Models/Table"));
-const user = __importStar(require("../Models/User"));
 const express = require('express');
 const index_1 = require("../index");
 exports.tablesRouter = express.Router();
@@ -39,17 +38,11 @@ exports.tablesRouter.get("/", (req, res) => {
         });
     }
     else {
-        user.getModel().findOne({ email: req.query.email }).then((user) => {
-            if (user) {
-                table.getModel().find({
-                    $or: [
-                        { waiter: user._id },
-                        { isFree: true }
-                    ]
-                }).sort({ number: 1 }).then((tables) => {
-                    res.status(200).json(tables);
-                });
-            }
+        table.getModel().find({ $or: [
+                { waiter: req.query.email },
+                { isFree: true }
+            ] }).sort({ number: 1 }).then((tables) => {
+            res.status(200).json(tables);
         }).catch((err) => {
             res.status(500).send('DB error: ' + err);
         });
@@ -85,34 +78,25 @@ exports.tablesRouter.delete('/:tableid', (req, res) => {
     console.log('DELETE request for items related to table: ' + req.params.tableid);
 });
 exports.tablesRouter.put("/:number", (req, res) => {
-    user.getModel().findOne({ email: req.query.email }).then((user) => {
-        if (user != null) {
-            console.log(user._id);
-            const filter = {
-                number: req.params.number
-            };
-            let update = {};
-            console.log(req.query.action);
-            if (req.query.action == 'occupy')
-                update = {
-                    isFree: false,
-                    waiter: user._id
-                };
-            else if (req.query.action == 'free')
-                update = {
-                    isFree: true,
-                    waiter: null
-                };
-            table.getModel().findOneAndUpdate(filter, update, { new: true }).then((table) => {
-                notify();
-                return res.status(200).send("Ok, table occupied: " + table);
-            }).catch((err) => {
-                return res.status(500).send('DB error: ' + err);
-            });
-        }
-        else {
-            console.log("sticazzi");
-        }
+    const filter = {
+        number: req.params.number
+    };
+    let update = {};
+    if (req.query.action == 'occupy')
+        update = {
+            isFree: false,
+            waiter: req.query.email
+        };
+    else if (req.query.action == 'free')
+        update = {
+            isFree: true,
+            waiter: null
+        };
+    table.getModel().findOneAndUpdate(filter, update, { new: true }).then((table) => {
+        notify();
+        return res.status(200).send("Ok, table occupied: " + table);
+    }).catch((err) => {
+        return res.status(500).send('DB error: ' + err);
     });
 });
 function notify() {
