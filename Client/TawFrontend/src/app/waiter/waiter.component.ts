76ed6a557b756ffb158228a3779eccdd8ae3794b
mizzet1 <riccardo.accts@gmail.com>
2023-06-20 18:02:59 +0200

    background

--- Client/TawFrontend/src/app/waiter/waiter.component.ts
+++ Client/TawFrontend/src/app/waiter/waiter.component.ts
@@ -10,38 +10,42 @@ import {ToastrService} from "ngx-toastr";
 @Component({
   selector: 'app-waiter',
   template: `
-      <div class="container" style="width: 900px; margin-top: 20px;">
-          <h2 class="text-center m-5">Tables List</h2>
+    <div class="container" style="width: 900px; margin-top: 20px; ">
+      <h2 class="text-center m-5">Tables List</h2>
 
-          <table class="table table-striped table-bordered">
-              <thead>
-              <tr>
-                  <th>Table number</th>
-                  <th>Seats</th>
-                  <th>State</th>
-                  <th>Actions</th>
-              </tr>
-              </thead>
+      <table class="table table-striped table-bordered" style="background: #ffffff;">
+        <thead>
+        <tr>
+          <th>Table number</th>
+          <th>Seats</th>
+          <th>State</th>
+          <th>Actions</th>
+        </tr>
+        </thead>
 
-              <tbody>
-              <tr *ngFor="let table of tables">
-                  <td>{{table.number}}</td>
-                  <td>{{table.seats}}</td>
-                  <td *ngIf="table.isFree == false">Occupied</td>
-                  <td *ngIf="table.isFree == false">
-                      <button *ngIf="table.waiter==getEmail()" class="btn btn-info" [routerLink]="['/menu', {'number': table.number}]">Add order</button>
-                      &nbsp;
-                      <button *ngIf="table.waiter==getEmail()" class="btn btn-primary" [routerLink]="['/orders', {'number': table.number}]">Orders status</button>
-                      &nbsp;
-                  </td>
-                  <td *ngIf="table.isFree == true">Free</td>
-                  <td *ngIf="table.isFree == true">
-                      <button class="btn btn-warning" (click)="occupyTable(table.number)">Occupy table</button> &nbsp;
-                  </td>
-              </tr>
-              </tbody>
-          </table>
-      </div>
+        <tbody>
+        <tr *ngFor="let table of tables">
+          <td>{{table.number}}</td>
+          <td>{{table.seats}}</td>
+          <td *ngIf="table.isFree == false">Occupied</td>
+          <td *ngIf="table.isFree == false">
+            <button *ngIf="table.waiter==getEmail()" class="btn btn-info"
+                    [routerLink]="['/menu', {'number': table.number}]">Add order
+            </button>
+            &nbsp;
+            <button *ngIf="table.waiter==getEmail()" class="btn btn-primary"
+                    [routerLink]="['/orders', {'number': table.number}]">Orders status
+            </button>
+            &nbsp;
+          </td>
+          <td *ngIf="table.isFree == true">Free</td>
+          <td *ngIf="table.isFree == true">
+            <button class="btn btn-warning" (click)="occupyTable(table.number)">Occupy table</button> &nbsp;
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   `
 })
 export class WaiterComponent implements OnInit {
